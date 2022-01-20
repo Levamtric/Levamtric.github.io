99e1bd3ba458c9e75482a49c37e96566f7d64195
mbeaver49 <manvel.beaver@protonmail.com>
2022-01-20 14:00:46 -0600

    [Add] Website init for version 1.0

---
+++ js/scripts.js
@@ -0,0 +1,70 @@
+/*!
+* Start Bootstrap - Freelancer v7.0.5 (https://startbootstrap.com/theme/freelancer)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 72,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+var survey_stock_image = 'https://images.pexels.com/photos/1188532/pexels-photo-1188532.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260';
+var coach_stock_image = 'https://images.pexels.com/photos/7207359/pexels-photo-7207359.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260';
+
+var modal_survey_stock_image = 'https://images.pexels.com/photos/1624600/pexels-photo-1624600.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260';
+var modal_coach_stock_image = 'https://images.pexels.com/photos/3184328/pexels-photo-3184328.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260';
+
+function setStockImage(idTag, imageURL) {
+    return document.getElementById(idTag).setAttribute('src', imageURL),
+        document.getElementById(idTag).setAttribute('alt', imageURL);
+}
+
+setStockImage('body-surveyIMG', survey_stock_image);
+setStockImage('modal-surveyIMG', modal_survey_stock_image);
+setStockImage('body-coachIMG', coach_stock_image);
+setStockImage('modal-coachIMG', modal_coach_stock_image);
